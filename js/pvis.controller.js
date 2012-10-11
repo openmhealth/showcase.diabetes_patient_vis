f36c79226b3d0b4e837260ce070d71c5b38a421b
Cameron Ketcham <cketcham@gmail.com>
2012-10-11 16:09:26 -0700

    formatting fixes

--- js/pvis.controller.js
+++ js/pvis.controller.js
@@ -3,39 +3,37 @@ pvis.controller = function(){
 
   $(document).ready(function(){
 
-		//init ohmage
-		omh.init("https://showcase.omh.io/app","PatienVis")
-    
+    //init ohmage
+    omh.init("https://showcase.omh.io/app","PatienVis")
+
     //login
     $('#loginButton').click(self.signIn)
     $('#logoutButton').click(omh.logout)
-		
-		
+
     if(!omh.token())
       pvis.ui.showLoginDialog()
     else{
-			$("#username").text(omh.username());
-			
-			$.mobile.showPageLoadingMsg();
-			
-			$.each(omh.payloads, function(k,v) {
-		    omh.read(
-		    	v.payload_id, 
-					v.version,
-					{success:function(res){
-		        console.log("read response",res)
-						omh.data[k] = res.data;
-						if(omh.data.pam && omh.data.runkeeper && omh.data.notes && omh.data.food && omh.data.sleep && omh.data.glucose && omh.data.weight) {
-							$.mobile.hidePageLoadingMsg();
-							pvis.plot(pvis.cmp);
-						}
-		      },
-					failure:function(e) {
-						alert("There was a server error " + e.responseText);
-					}
-				})
-			});
+      $("#username").text(omh.username());
 
+      $.mobile.showPageLoadingMsg();
+
+      $.each(omh.payloads, function(k,v) {
+        omh.read(
+          v.payload_id,
+          v.version,
+          {success:function(res){
+            console.log("read response",res)
+            omh.data[k] = res.data;
+            if(omh.data.pam && omh.data.runkeeper && omh.data.notes && omh.data.food && omh.data.sleep && omh.data.glucose && omh.data.weight) {
+              $.mobile.hidePageLoadingMsg();
+              pvis.plot(pvis.cmp);
+            }
+          },
+          failure:function(e) {
+            alert("There was a server error " + e.responseText);
+          }
+        })
+      });
     }
   })
 
@@ -49,15 +47,15 @@ pvis.controller = function(){
 
     $('#loginDialog .msg').text('Signing In...')
 
-		omh.authenticate(user,password,{
-		  success:function(){
-				window.location.reload();
-		  },
-		  failure:function(){
-  	  	$('#loginDialog .msg').css('color','red').
-  	    	text('Incorrect user name or password. Please try again.')
-  	    }
-		})
+    omh.authenticate(user,password,{
+      success:function(){
+        window.location.reload();
+      },
+      failure:function(){
+        $('#loginDialog .msg').css('color','red').
+        text('Incorrect user name or password. Please try again.')
+      }
+    })
     return false
   }
 
