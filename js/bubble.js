e31f7b71f4e73d0900517bda511f99d0ee223323
Cameron Ketcham <cketcham@gmail.com>
2012-10-23 15:41:37 -0700

    updated bubble so it will always fit on screen as best it can. (did not implement any word wrapping)

--- js/bubble.js
+++ js/bubble.js
@@ -6,7 +6,7 @@ function bubble(g, dx) {
 		.attr("fill", "#D0D0D0")
 		.attr("stroke-width",1)
 		.attr("dx", dx)
-               .attr("filter","url(#dropshadow)")
+		.attr("filter","url(#dropshadow)")
 		
 
 	this.addText = function(lines) {
@@ -23,24 +23,54 @@ function bubble(g, dx) {
 		text.selectAll("tspan").attr("dx","-"+text.node().getBBox().width/2);
 
 		var noteBox=text.node().getBBox()
-			
-		this.path.attr("d",bubbleContainer( {
-			"start": dx - noteBox.width/2,
-			"width": noteBox.width,
-			"height": noteBox.height - 15,
-			"corner": 10
-			})
-		);
+
+    var bounds = {
+      "start": dx - noteBox.width/2,
+      "width": noteBox.width,
+      "height": noteBox.height - 15,
+      "corner": 10,
+      "min":0,
+      "max":440
+    };
+
+    bounds.offset = 0;
+
+    if(bounds.start<bounds.min) {
+      bounds.offset = -bounds.start;
+      bounds.start = bounds.min;
+    } else if(bounds.start+bounds.width>bounds.max) {
+      var oldstart = bounds.start;
+      bounds.start = bounds.max - bounds.width
+      bounds.offset = bounds.start - oldstart
+    }
 		
+    text.selectAll("tspan").attr("x",bounds.start + bounds.width/2)
+		this.path.attr("d",bubbleContainer(bounds));
 		this.group.attr("transform", "translate(0," + (140 - noteBox.height) + ")");
 	}
 }
 
 function bubbleContainer(x) {
 	var tickWidth = 20;
+
+  var leftSideFix = -((x.width-tickWidth)/2 - x.offset);
+  if(leftSideFix < 0) leftSideFix = 0;
+
+  var half = (x.width-tickWidth)/2
+  var left = half - x.offset;
+  var right = half + x.offset;
+
+  if(left < 0) {
+    right = right - left - tickWidth;
+    left = 0;
+  } else if(right < 0) {
+    left = left - right - tickWidth;
+    right = 0;
+  }
+
 	return "M " + x.start + " 0 l " + x.width + " 0 "+corner(x.corner,x.corner) 
 		+ "l 0 "+ x.height + corner(-x.corner,x.corner) 
-		+ "l " + -((x.width-tickWidth)/2) + " 0 l -10 10 l -10 -10 l "+ -((x.width-tickWidth)/2) + " 0 " + corner(-x.corner,-x.corner)
+		+ "l " + -right + " 0 l -10 10 l -10 -10 l "+ -left + " 0 " + corner(-x.corner,-x.corner)
 		+ "l 0 " + -x.height + corner(x.corner,-x.corner);
 }
 
